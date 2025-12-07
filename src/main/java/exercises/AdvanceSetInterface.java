991cb4d26bac81c811a1eb5f3bcf205630b8183d
elevy16 <elevy16@student.touro.edu>
2025-12-07 00:05:21 -0500

    Finish Homework 8 on Collections

--- src/main/java/exercises/AdvanceSetInterface.java
+++ src/main/java/exercises/AdvanceSetInterface.java
@@ -7,12 +7,32 @@ import edu.touro.mcon264.apps.collections.CollectionInterface;
  * @param <T>
  */
 public interface AdvanceSetInterface <T> extends CollectionInterface<T> {
-/*
- Starter code the exercise 6
- Desig signatures for the following methods:
-    union()
-    intersection()
-    difference()
- */
+
+    /**
+     * Creates and returns a new set that contains all elements from
+     * this set and the other set (no duplicates).
+     *
+     * @param other another set to combine with this one
+     * @return a new set representing the union of the two sets
+     */
+    AdvanceSetInterface<T> union(AdvanceSetInterface<T> other);
+
+    /**
+     * Creates and returns a new set that contains only the elements
+     * that appear in both this set and the other set.
+     *
+     * @param other another set to compare with
+     * @return a new set representing the intersection of the two sets
+     */
+    AdvanceSetInterface<T> intersection(AdvanceSetInterface<T> other);
+
+    /**
+     * Creates and returns a new set that contains the elements that
+     * are in this set but not in the other set.
+     *
+     * @param other another set whose elements should be removed from this set
+     * @return a new set representing the difference between the sets
+     */
+    AdvanceSetInterface<T> difference(AdvanceSetInterface<T> other);
 
 }
